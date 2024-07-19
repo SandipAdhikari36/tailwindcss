d8a3794e25d7a34ffcbc3ec034e935610849829d
SandipAdhikari36 <sandipadhikari034@gmail.com>
2024-07-19 16:59:24 +0545

    my first css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        ashish: '33Ff39',
+        om:'red',
+      },
+    },
+  },
+  plugins: [],
+}
+
